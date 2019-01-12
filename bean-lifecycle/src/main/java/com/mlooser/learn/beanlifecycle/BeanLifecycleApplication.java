d522201065a2877a724665613a09c1b7a263c38f
mlooser <MarcinLusa@192.168.0.24>
2019-01-12 13:19:06 +0100

    commiter test

--- bean-lifecycle/src/main/java/com/mlooser/learn/beanlifecycle/BeanLifecycleApplication.java
+++ bean-lifecycle/src/main/java/com/mlooser/learn/beanlifecycle/BeanLifecycleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeanLifecycleApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(BeanLifecycleApplication.class, args);
 	}
