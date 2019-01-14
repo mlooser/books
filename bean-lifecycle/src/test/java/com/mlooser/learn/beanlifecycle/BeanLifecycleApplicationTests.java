fa0131fdbb555d44dc6aef153627a69b031fcc89
mlooser <>
2019-01-14 20:34:38 +0100

    commiter test

--- bean-lifecycle/src/test/java/com/mlooser/learn/beanlifecycle/BeanLifecycleApplicationTests.java
+++ bean-lifecycle/src/test/java/com/mlooser/learn/beanlifecycle/BeanLifecycleApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BeanLifecycleApplicationTests {
-
+//comit test
 	@Test
 	public void contextLoads() {
 	}
