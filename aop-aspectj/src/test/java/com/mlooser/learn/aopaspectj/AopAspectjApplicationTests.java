a3aa0ead33fd5f4ba1935ccb2c0c1d240466d06c
mlooser <mlooser@gmail.com>
2019-01-30 18:25:50 +0100

    Additional files for aop-aspectj.

---
+++ aop-aspectj/src/test/java/com/mlooser/learn/aopaspectj/AopAspectjApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mlooser.learn.aopaspectj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AopAspectjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
