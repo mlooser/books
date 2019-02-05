d385a4d8174ea5afccc90b8168d82a75ff344b49
mlooser <mlooser@gmail.com>
2019-02-05 17:06:36 +0100

    [AuthorizationService] Additional files.

---
+++ authorization-service/src/test/java/com/mlooser/learn/eagleeye/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mlooser.learn.eagleeye.authorizationservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthorizationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
