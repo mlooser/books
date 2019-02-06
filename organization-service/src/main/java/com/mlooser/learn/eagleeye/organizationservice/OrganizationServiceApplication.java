f8fa30626e46688b021f926830d53cdef5ead169
mlooser <mlooser@gmail.com>
2019-02-06 10:04:18 +0100

    [OrganizationService] Enable OAuth2 authorization requirement to access organizations.

--- organization-service/src/main/java/com/mlooser/learn/eagleeye/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/mlooser/learn/eagleeye/organizationservice/OrganizationServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import com.mlooser.learn.eagleeye.organizationservice.repositories.OrganizationRepository;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
