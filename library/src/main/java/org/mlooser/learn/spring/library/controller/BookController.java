6a6cc33e8dd7d1a77260c295306e7765419e3182
mlooser <mlooser@gmail.com>
2020-03-17 14:40:32 +0100

    [library] book details

--- library/src/main/java/org/mlooser/learn/spring/library/controller/BookController.java
+++ library/src/main/java/org/mlooser/learn/spring/library/controller/BookController.java
@@ -4,6 +4,7 @@ import org.mlooser.learn.spring.library.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
@@ -18,5 +19,14 @@ public class BookController {
     public String getAllBooks(Model model){
         model.addAttribute("books", bookService.findAll());
         return "books/list";
+    }
+
+    @GetMapping(value = "/books.html", params = "isbn")
+    public String getBook(@RequestParam("isbn") String isbn, Model model){
+        bookService
+                .find(isbn)
+                .ifPresent(book->model.addAttribute("book", book));
+
+        return "books/details";
     }
 }
