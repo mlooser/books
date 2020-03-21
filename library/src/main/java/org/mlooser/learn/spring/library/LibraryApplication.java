10be93fe20bff3abbb8c9dbc322b4839847af1b0
mlooser <mlooser@gmail.com>
2020-03-21 19:37:50 +0100

    [library] change locale

--- library/src/main/java/org/mlooser/learn/spring/library/LibraryApplication.java
+++ library/src/main/java/org/mlooser/learn/spring/library/LibraryApplication.java
@@ -5,12 +5,22 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
-public class LibraryApplication {
+public class LibraryApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 
 	@Bean
@@ -22,5 +32,15 @@ public class LibraryApplication {
 			bookService.create(
 					new Book("God's General","222", "Robert Larson"));
 		};
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor(){
+		return new LocaleChangeInterceptor();
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		return new CookieLocaleResolver();
 	}
 }
