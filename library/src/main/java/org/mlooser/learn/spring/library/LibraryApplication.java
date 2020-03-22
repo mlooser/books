013c3aa5ce6057d8ca607dc74cd28b921e75dc9a
mlooser <mlooser@gmail.com>
2020-03-22 18:32:33 +0100

    [library] redirect http to https

--- library/src/main/java/org/mlooser/learn/spring/library/LibraryApplication.java
+++ library/src/main/java/org/mlooser/learn/spring/library/LibraryApplication.java
@@ -1,15 +1,21 @@
 package org.mlooser.learn.spring.library;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mlooser.learn.spring.library.service.BookService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import sun.security.util.SecurityConstants;
 
 @SpringBootApplication
 public class LibraryApplication implements WebMvcConfigurer {
@@ -31,6 +37,34 @@ public class LibraryApplication implements WebMvcConfigurer {
 
 			bookService.create(
 					new Book("God's General","222", "Robert Larson"));
+		};
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory  tomcatServletWebServerFactory(){
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addAdditionalTomcatConnectors(httpConnector());
+		factory.addContextCustomizers(securityCustomizer());
+		return factory;
+	}
+
+	private Connector httpConnector(){
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
+	private TomcatContextCustomizer securityCustomizer(){
+		return context -> {
+			SecurityConstraint securityConstraint = new SecurityConstraint();
+			securityConstraint.setUserConstraint("CONFIDENTAL");
+			SecurityCollection securityCollection = new SecurityCollection();
+			securityCollection.addPattern("/*");
+			securityConstraint.addCollection(securityCollection);
+			context.addConstraint(securityConstraint);
 		};
 	}
 
