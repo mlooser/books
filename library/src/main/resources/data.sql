1150e33cbf9a407d8cc23f0ef98884b1512c95bf
mlooser <mlooser@gmail.com>
2021-01-18 12:18:42 +0100

    cleanups

--- library/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO USERS(USERNAME, PASSWORD, ENABLED) VALUES
-('admin2', '{noop}aaa', 1),
-('normal2', '{noop}nnn', 1);
-
-INSERT INTO AUTHORITIES(USERNAME, AUTHORITY) VALUES
-('admin2', 'ADMIN'),
-('admin2', 'USER'),
-('normal2', 'USER');
-
